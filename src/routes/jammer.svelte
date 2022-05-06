d27f552abdf87591c29227a6192ac09ddafb83cf
Marco-Cellini <marco.cellini@istitutoagnelli.it>
2022-05-06 12:48:27 +0200

    fix typo

--- src/routes/jammer.svelte
+++ src/routes/jammer.svelte
@@ -11,8 +11,7 @@ import Article from './lib/article.svelte';
 <body>
     <Article 
     header="Il jammer"
-    meta = "Scritto da Andrea    header="Il jammer"
-    Canale e Marco Cellini Venerdì 6 Maggio 2022"
+    meta = "Scritto da Andrea Canale e Marco Cellini Venerdì 6 Maggio 2022"
     subtitle = "Il jammer"
     text = "Il jammer è un disturbatore di segnale"
     ></Article>
