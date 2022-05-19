4e0952d8bb1fbf942eecef60fc031cb1c79fcfc8
Andrea055o <vivanco11.ac@gmail.com>
2022-05-19 16:22:39 +0200

    fix slide

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,7 @@
     import { onMount } from "svelte";
   import "uikit/dist/css/uikit.css";
   import "uikit/dist/js/uikit.js";
+  import Slide from './lib/slide_home.svelte'
   let images= [];
   onMount(() => {
     fetch(window.location.href + "img/slide/img.json")
@@ -24,56 +25,8 @@
   </h1>
   <h1>&nbsp;</h1>
 
-  <div
-  class="uk-position-relative uk-visible-toggle uk-light"
-  tabindex="-1"
-  uk-slideshow="min-height: 300; max-height: 600"
-  hidden={false}
->
-  <ul class="uk-slideshow-items">
-        <a href="/pericoli/fake-news/">
-          <li>
-          <img src="/img/slide/fake_news_pc.png" alt="">
-        </li>
-      </a>
-        <a href="/strumenti/jammer">
-          <li>
-          <img src="/img/slide/jammer.png" alt="">
-        </li>
-      </a>
-      <a href="/strumenti/raspberry">
-        <li>
-          <img src="/img/slide/raspberry.png" alt="">
-        </li>
-      </a>
-      <a href="/strumenti/rubber-ducky">
-        <li>
-          <img src="/img/slide/rubber_ducky.png" alt="">
-        </li>
-      </a>
-        <a href="/crittografia/sniffing">
-          <li>
-          <img src="/img/slide/sniffing.jpeg" alt="dssa">
-        </li>
-        
-      </a>
-  </ul>
-
-  <a
-    class="uk-position-center-left uk-position-small uk-hidden-hover"
-    href="#"
-    alt=""
-    uk-slidenav-previous
-    uk-slideshow-item="previous"
-  />
-  <a
-    class="uk-position-center-right uk-position-small uk-hidden-hover"
-    href="#"
-    alt=""
-    uk-slidenav-next
-    uk-slideshow-item="next"
-  />
-</div>
+ 
+<Slide/>
 
   <br />
 </section>
--- src/routes/lib/slide_home.svelte
+++ src/routes/lib/slide_home.svelte
@@ -1,4 +1,96 @@
 <script lang="ts">
+  import './slide.css';
+</script>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
+</head>
+<body>
+<div align="center">
+<div class="slideshow-container" align="center">
+
+<div class="mySlides fade">
+  <a class="prev" onclick="plusSlides(-1)">❮</a>
+  <img src="/img/slide/fake_news_pc.png" >
+  <h1>&nbsp;</h1>
+  <div class="text">Fake news</div>
+</div>
+
+<div class="mySlides fade">
+  <a class="prev" onclick="plusSlides(-1)">❮</a>
+  <img src="/img/slide/jammer.png" >
+  <h1>&nbsp;</h1>
+  <div class="text">Il jammer</div>
+</div>
+
+<div class="mySlides fade">
+  <a class="prev" onclick="plusSlides(-1)">❮</a>
+  <img src="/img/slide/raspberry.png" >
+  <h1>&nbsp;</h1>
+  <div class="text">Il raspberry</div>
+</div>
+
+<div class="mySlides fade">
+  <a class="prev" onclick="plusSlides(-1)">❮</a>
+  <img src="/img/slide/rubber_ducky.png" >
+  <h1>&nbsp;</h1>
+  <div class="text">Una rubber ducky</div>
+</div>
+
+<div class="mySlides fade">
+  <a class="prev" onclick="plusSlides(-1)">❮</a> 
+  <img src="/img/slide/sniffing.jpeg" >
+  <h1>&nbsp;</h1>
+  <div class="text">Schema dello sniffing</div>
+</div>
+
+
+<a class="next" onclick="plusSlides(1)">❯</a>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+  <span class="dot" onclick="currentSlide(4)"></span> 
+  <span class="dot" onclick="currentSlide(5)"></span> 
+</div>
+</div>
+<script>
+slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
 </script>
 
+</body>
+
+<style>
+  img{
+    width: 600px;
+  }
+</style>
