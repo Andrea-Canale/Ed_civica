abb1168d8fac00ab04eb7728192c88a15a3fbc04
Gioele-Mari <gioele.mari@istitutoagnelli.it>
2022-05-19 17:30:43 +0200

    add abusi

---
+++ src/routes/sicurezza/accessi-abusivi.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+    import Article from "../lib/article.svelte";
+  </script>
+  
+  <svelte:head>
+    <link rel="stylesheet" href="/css/jammer.css" />
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/purecss@1.0.1/build/base-min.css"
+    />
+  </svelte:head>
+  
+  <section>
+    <div class="container">
+      <Article
+        imgmaxwidth="500px"
+        header="Abusivo ad un sistema informatico"
+        meta="Scritto da Andrea Canale, Marco Cellini e Gioele Mari Domenica 19 Maggio 2022"
+        subtitle=""
+        text="L’accesso abusivo ad un sistema informatico fa parte di quei reati cosiddetti “reati informatici”. A livello internazionale non è stata data loro una definizione, tuttavia, possiamo definirli come “atti o fatti contrari alle norme penali vigenti, nei quali il computer viene coinvolto come oggetto o come strumento”.
+        Si suole suddividere questo tipo di reati in:"
+        imgcomment=""
+        imgsrc="/img/accesso-sistema-informatico.jpg"
+      />
+    </div>
+    <div align="center" class="container">
+
+  
+      <div align="left" class="container">
+        <ul>
+          <li class="text">Lista minima: prevede tutte quelle condotte ritenute necessariamente meritevoli di sanzioni penali. Il legislatore nazionale è sollecitato affinché inserisca delle specifiche disposizioni penali che puniscono quei comportamenti. Sono inseriti, ad esempio, la frode informatica e l’accesso abusivo ad un sistema informatico;</li>
+          <li class="text">
+            Lista facoltativa: contenente i casi in cui l’incriminazione è rimessa alla discrezionalità del legislatore, il quale potrà scegliere se punire o meno quelle condotte, come ad esempio lo spionaggio informatico;
+        </li>
+        </ul>
+      </div>
+  
+      
+  
+      <hr class="uk-divider-icon" />
+      <p class="text">
+        È stato il Comitato dei Ministri degli Stati membri dell’OCSE(Organizzazione per la cooperazione e lo sviluppo economico) a raggruppare le condotte delittuose nate a seguito dello sviluppo delle nuove tecnologie nella Raccomandazione n. R.  9 del 1989 e a suddividerle in due liste:
+       </p>
+       <div align="left" class="container">
+        <ul>
+          <li class="text">Propri: in mancanza di un computer il comportamento incriminato non è concepibile. Questo è il caso, ad esempio, dell’accesso abusivo ad un sistema informatico;</li>
+          <li class="text">
+            Impropri, in questi casi il computer è uno strumento utilizzato per porre in essere un’offesa tradizionale, pensiamo ad esempio alla diffamazione che avviene tramite social network;
+
+          </li>
+        </ul>
+      </div>
+      <h1><strong>Pena</strong></h1>
+      <div align="centre" class="container">
+        <p class="text">
+            Il reato può essere commesso da "chiunque" nell'ipotesi di cui al primo comma ed in tal caso la sanzione è fino a tre anni di reclusione.
+            Tuttavia è prevista un'aggravante speciale se il fatto dovesse essere commesso da pubblico ufficiale.
+            Prevede infatti la norma che "La pena è della reclusione da uno a cinque anni: se il fatto è commesso da un pubblico ufficiale o da un incaricato di un pubblico servizio, con abuso dei poteri, o con violazione dei doveri inerenti alla funzione o al servizio, o da chi esercita anche abusivamente la professione di investigatore privato, o con abuso della qualità di operatore del sistema".
+        </p>
+        
+  
+        <br />
+      </div>
+    </div>
+  </section>
+  
+  <style>
+    li {
+      color: white;
+    }
+  </style>
+  
