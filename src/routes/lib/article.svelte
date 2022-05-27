e3f2f82c6c5b426c7dcd8b01b5b18a8675f6c597
Andrea055o <vivanco11.ac@gmail.com>
2022-05-27 08:52:12 +0200

    fix img in phone

--- src/routes/lib/article.svelte
+++ src/routes/lib/article.svelte
@@ -20,13 +20,11 @@
   <h3 class="uk-article-meta meta">{meta}</h3>
 
   <p class="submeta">{subtitle}</p>
-  <div align="center">
+  <div class="container" align="center">
     <div uk-lightbox>
       <a href={imgsrc}>
         <img
           src={imgsrc}
-          class="pure-img"
-          style={"max-width: " + imgmaxwidth}
           alt={"foto di " + header}
         />
       </a>
