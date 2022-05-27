b03738da06d35a71381e81f35b3f55275fb03a26
Andrea055o <vivanco11.ac@gmail.com>
2022-05-27 09:47:01 +0200

    add optimizer of assets

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,40 @@
 // svelte.config.js
 import sveltePreprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-static";
-
+import viteImagemin from 'vite-plugin-imagemin';
+import viteCompression from 'vite-plugin-compression';
 export default {
+  plugins: [
+    viteCompression(),
+    viteImagemin({
+      verbose: true,
+      gifsicle: {
+        optimizationLevel: 10,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 10,
+      },
+      mozjpeg: {
+        quality: 70,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
+    }),
+  ],
   preprocess: sveltePreprocess(),
   kit: {
     adapter: adapter({
